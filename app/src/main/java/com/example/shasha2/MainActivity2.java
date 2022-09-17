c6d88a436a363e3a087a5f0b25a1bd3a92b46867
Ayamachii <ayaeyad87@gmail.com>
2022-09-17 15:24:45 +0200

    added onClickListeners for addtobasket

--- app/src/main/java/com/example/shasha2/MainActivity2.java
+++ app/src/main/java/com/example/shasha2/MainActivity2.java
@@ -1,14 +1,43 @@
 package com.example.shasha2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    AppCompatButton plus_btn;
+    AppCompatButton minus_btn;
+    TextView cnt;
+    public int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        plus_btn = findViewById(R.id.plus);
+        minus_btn = findViewById(R.id.minus);
+        cnt = findViewById(R.id.textView);
+
+        plus_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                cnt.setText(String.valueOf(count));
+            }
+        });
+
+        minus_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count--;
+                cnt.setText(String.valueOf(count));
+            }
+        });
+
     }
 }
